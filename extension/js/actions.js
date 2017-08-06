5986220bcb283c25d043cc4d39226518674b0e0f
bowei <bowei.han100@gmail.com>
2017-08-06 15:10:32 -0400

    able to send messages from popup to content script

--- extension/js/actions.js
+++ extension/js/actions.js
@@ -4,22 +4,36 @@
     var bg = chrome.extension.getBackgroundPage();
 
     var buttons = {
-        record: '#js-record',
-        stop: '#js-stop',
-        accept: '#js-accept',
-        reject: '#js-reject'
+        '#js-record' : "record",
+        '#js-stop' : "stop",
+        '#js-accept' : "accept",
+        '#js-reject' : "reject"
     };
 
     var initButtons = function(buttons) {
         var values = Object.values(buttons);
         var keys = Object.keys(buttons);
-        for (var i = 0; i < values.length; i++) {
-            $(values[i]).on('click', logButton);
+        for (var i = 0; i < keys.length; i++) {
+            $(keys[i]).on('click', logButton);
         }
     };
 
+    var responseCallback = function(info) {
+        // optional callback on response
+    };
+
+    // TODO: this is sending two messages at the moment
     var logButton = function(elem) {
         bg.console.log(elem);
+        chrome.tabs.query({
+            active: true,
+            currentWindow: true
+        }, function (tabs) {
+            chrome.tabs.sendMessage(
+                tabs[0].id,
+                { from: 'popup', subject: elem.currentTarget.id },
+                responseCallback);
+        });
     };
 
     initButtons(buttons);
--- extension/js/listeners.js
+++ extension/js/listeners.js
@@ -1,30 +1,41 @@
 (function() {
     'use strict';
 
-    var buildSelectorAncestry = function(target) {
-        var parents = [];
-        var element;
-        var entry;
+    chrome.runtime.onMessage.addListener(function (msg, sender, response) {
+        var action = msg.subject.slice(3); // only need what's after 'js-'
+        console.log(action);
+    });
 
-        for (element = target; element; element = element.parentNode) {
-            entry = element.tagName.toLowerCase();
-            if (entry === "html") {
-                break;
+    var History = function() {
+        this.actions = [];
+        this.buildSelectorAncestry = function(target) {
+            var parents = [];
+            var element;
+            var entry;
+            for (element = target; element; element = element.parentNode) {
+                entry = element.tagName.toLowerCase();
+                if (entry === "html") { break; }
+                if (element.className) { entry += "." + element.className.replace(/ /g, '.'); }
+                if (element.id) { entry += '#' + element.id; }
+                parents.push(entry);
             }
-            if (element.className) {
-                entry += "." + element.className.replace(/ /g, '.');
-            }
-            if (element.id) {
-                entry += '#' + element.id;
-            }
-            parents.push(entry);
-        }
-
-        parents.reverse();
-        return parents.join(" ");
+            parents.reverse();
+            return parents.join(" ");
+        };
+        this.addClickAction = function(target) {
+            this.actions.push({
+                action: "click",
+                selector: this.buildSelectorAncestry(target)
+            });
+        };
+        this.clearHistory = function() {
+            this.actions.length = 0;
+        };
     };
 
+    var history = new History();
+
     $('body').on('click', function(evt) {
-        console.log(buildSelectorAncestry(evt.target));
+        history.addClickAction(evt.target);
     });
 })();
